2f380b12526c9367116e2faaf0131336860b5709
Richard DELAPLACE <rdelaplace@yueyehua.net>
2019-01-02 18:35:03 +0100

    Add few languages to recognition

--- src/nlp/parser/languagerecognitionparser.cr
+++ src/nlp/parser/languagerecognitionparser.cr
@@ -3,10 +3,43 @@ module NLP
     module LanguageRecognitionParser
       extend self
 
+      def check_charsets(str : String, charsets : String)
+        str.match(%r((#{charsets}))).nil? ? false : true
+      end
+
+      def is_armenian?(str : String)
+        check_charsets(str, "\\p{Armenian}")
+      end
+
+      def is_cyrillic?(str : String)
+        check_charsets(str, "\\p{Cyrillic}")
+      end
+
+      def is_georgian?(str : String)
+        check_charsets(str, "\\p{Georgian}")
+      end
+
+      def is_greek?(str : String)
+        check_charsets(str, "\\p{Greek}")
+      end
+
       def is_japanese?(str : String)
-        jp_charsets = "\\p{Hiragana}|\\p{Katakana}|\\p{Han}|" \
-          "[\\x{3000}-\\x{303F}]|[\\x{FF5F}-\\x{FF9F}]"
-        str.match(%r((#{jp_charsets}))).nil? ? false : true
+        check_charsets(
+          str,
+          "\\p{Hiragana}|\\p{Katakana}|[\\x{3000}-\\x{303F}]"
+        )
+      end
+
+      def is_khmer?(str : String)
+        check_charsets(str, "\\p{Khmer}")
+      end
+
+      def is_korean?(str : String)
+        check_charsets(str, "\\p{Hangul}")
+      end
+
+      def is_serbian?(str : String)
+        is_cyrillic?(str) && ! check_charsets(str, "ъщяюй")
       end
     end
   end
